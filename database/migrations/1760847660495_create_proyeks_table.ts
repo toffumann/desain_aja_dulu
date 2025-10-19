745a3fcb3b707840eda865cfccb33062fab803ea
MarselDev <marselcristian10@gmail.com>
2025-10-19 14:32:55 +0800

    selesai migration

--- database/migrations/1760847660495_create_proyeks_table.ts
+++ database/migrations/1760847660495_create_proyeks_table.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('id_klien').unsigned().references('id').inTable('klien').onDelete('CASCADE').notNullable()
-      table.integer('id_layanan').unsigned().references('id').inTable('layanan').notNullable()
+      table.integer('id_klien').unsigned().references('id').inTable('kliens').onDelete('CASCADE').notNullable()
+      table.integer('id_layanan').unsigned().references('id').inTable('layanans').notNullable()
       table.text('catatan_klien').nullable()
       table.enum('status_proyek', ['Baru', 'Proses', 'Revisi', 'Selesai', 'Batal']).defaultTo('Baru').notNullable()
       table.date('tanggal_mulai').notNullable()
